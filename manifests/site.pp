44b62a74e4e84ca39d83e4809f8741e92a84f97b
litalum <58083275+litalum@users.noreply.github.com>
2019-11-22 14:00:20 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,20 @@
 node default {
-
+  file { '/root/README':
+    ensure => file,
+    content => 'This is a readme',
+    owner => 'root',
+  
+  }
 }
 node 'master.pupper.vm' {
   include role::master_server
+}
+
+node /^web/ {
+  include role::app_server
+}
+
+
+node /^db/ {
+  include role::db_server
 }
